b68d9316ac831f3784ee76af1bf704e1470406d8
Grady Songe <Gradysonge70@gmail.com>
2025-02-18 18:35:24 -0500

    Mardi_front end

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import clsx from 'clsx';
 
 const user = {
   name: 'Samir Elouasbi',
-  role: 'Administrateur ',
+  role: 'Administrateur la cité',
 
   avatar: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
 };
