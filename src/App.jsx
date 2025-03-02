89813bc5b082930534433b8424ea5c279c4af7c7
David-Nasial <davidbasola56@gmail.com>
2025-03-02 15:52:54 -0500

    Page acceuil 80% accomplie

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Professors from './components/Professors';
 import Calendar from './components/Calendar/Calendar';
+import Accueil from './components/Accueil/Accueil';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Sidebar />
         <main className="flex-1 overflow-hidden">
           <Routes>
-            <Route path="/" element={<div>Dashboard  a developper plus tard </div>} />
+            <Route path="/" element={<Accueil/>} />
             <Route path="/calendar" element={<Calendar />} />
             <Route path="/professors" element={<Professors/>} />
           </Routes>
